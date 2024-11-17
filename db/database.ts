849d4f615da6d9edaa83d2b8fbfba951e289df7d
Dev Cyth <hydra@Devs-MacBook-Pro.local>
2024-11-17 18:13:41 +0100

    deploy github actions

--- db/database.ts
+++ db/database.ts
@@ -1,13 +1,16 @@
 import { Pool } from "pg";
 import 'dotenv/config'
 
-
+console.log(process.env)
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: String(process.env.DB_PASSWORD),
-  port: 5432,
+  port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false, // Set to false if you don't have a CA certificate, use true with valid certificates for more security
+  },
 });
 
 export default pool;
